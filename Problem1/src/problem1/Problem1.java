e1ae9e9aa9470be7903d6ca58de37d5e9f83d5fb
Michael <Michael@Michael-PC.mit.edu>
2018-09-05 18:56:19 -0400

    trying

--- Problem1/src/problem1/Problem1.java
+++ Problem1/src/problem1/Problem1.java
@@ -1,5 +1,18 @@
 package problem1;
 
 public class Problem1 {
-//just one more test
+	public static void main(String[] args) {
+	int sum = 0;
+	
+	
+	for(int i = 0; i > 1000; i++) {
+		if(i % 3 == 0) {
+			sum += i;
+		} else if(i % 5 == 0) {
+			sum += i;
+		}
+	}
+	
+	System.out.println(sum);
+	}
 }
